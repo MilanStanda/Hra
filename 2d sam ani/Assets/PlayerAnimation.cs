d0a9c4dfdc8e13609382a2877c21ae6a1042ab69
StanleyFu <standa.fuka@gmail.com>
2020-01-11 16:05:31 +0100

    Hra v1.0

---
+++ 2d sam ani/Assets/PlayerAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerAnimation : MonoBehaviour
+{
+    bool pohyb = false;
+    public CharacterController2D controller;
+    bool Animace = false;
+    
+
+
+    void Update()
+    {
+
+      if(!pohyb)
+        {
+            
+        }
+        
+    }
+}
---
+++ 2d sam ani/Assets/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;   // vždy když napíšem controller tak to bude odkaz na to co je v řádku v unity u player movementu
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+    
+
+    // Update se obnovuje každou sekundu
+    void Update()
+    {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+            else if (Input.GetButtonUp("Crouch"))
+             {
+                 crouch = false;
+             }
+              
+    }
+
+    void FixedUpdate()
+    {
+        //pohyb hráče
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);   
+        jump = false;    //musí být protože jinak bychom skákali do nekonečna
+    }
+
+
+
+
+
+
+
+}
